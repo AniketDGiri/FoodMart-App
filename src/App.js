6ba14ede1f4a6deec67452a6aa5bc71fdd457673
AnkDGiri777 <aniketgiri770@gmail.com>
2023-08-08 11:29:53 +0530

    fix: added testing files

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactasdsad
+          Learn React
         </a>
       </header>
     </div>
