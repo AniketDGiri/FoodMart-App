2488ad33cf3779886564a0dd86078a15d475f80b
AnkDGiri777 <aniketgiri770@gmail.com>
2023-08-22 13:28:43 +0530

    feat: added login name on dropdown menu

--- src/components/Authentication/Register.js
+++ src/components/Authentication/Register.js
@@ -70,7 +70,7 @@ export const Register = () => {
             type="name"
             id="name"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="Shubham Sarda"
+            placeholder="Aniket Giri"
             required
             autoComplete="off"
           />
@@ -86,7 +86,7 @@ export const Register = () => {
             type="email"
             id="email"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="shubham@example.com"
+            placeholder="aniket@example.com"
             required
             autoComplete="off"
           />
--- src/components/Layout/Header/DropDownLoggedIn.js
+++ src/components/Layout/Header/DropDownLoggedIn.js
@@ -1,6 +1,12 @@
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getSessionData } from "../../../services/sessionDetails";
+import apiCalls from "../../../services/apiCalls";
+import { toast } from "react-toastify";
 
 export const DropdownLoggedIn = ({ setIsDropDownn }) => {
+  const [userData, setUserData] = useState({});
+
   const navigate = useNavigate();
   const onLogInHandler = () => {
     navigate("/");
@@ -9,13 +15,45 @@ export const DropdownLoggedIn = ({ setIsDropDownn }) => {
     setIsDropDownn(false);
   };
 
+  const { accessToken, userId } = getSessionData();
+
+  useEffect(() => {
+    if (accessToken) {
+      const getUserDetails = async () => {
+        try {
+          const url = `${process.env.REACT_APP_URL}:${process.env.REACT_APP_PORT_NO}/600/users/${userId}`;
+
+          const res = await apiCalls({
+            method: "get",
+            url: url,
+            headers: { Authorization: `Bearer ${accessToken}` },
+          });
+
+          setUserData(res.data);
+        } catch (err) {
+          toast.error(`Error Fetching user Data`, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        }
+      };
+      getUserDetails();
+    }
+  }, [accessToken, userId]);
+
   return (
     <div
       id="dropdownAvatar"
       className="select-none	absolute top-10 right-0 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
     >
       <div className="py-3 px-4 text-sm text-gray-900 dark:text-white">
-        <div className="font-medium truncate">shubham@example.com</div>
+        <div className="font-medium truncate">{userData.email || ""}</div>
       </div>
       <ul
         className="py-1 text-sm text-gray-700 dark:text-gray-200"
